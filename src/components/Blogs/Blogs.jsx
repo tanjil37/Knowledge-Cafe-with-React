d814f1b97a134b577d1fa777b9f5da9c2be4d832
Tanjilul Islam <tanjilulislam37@gmail.com>
2025-01-03 00:17:52 +0600

    many components basic structure are complet and video 3 complete

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,15 +1,21 @@
 import { useEffect } from "react";
 
 
+
+
+
+
+
 const Blogs = () => {
-    const [blogs, setBlogs] = useState([]);
+    //const [blogs, setBlogs] = useState([]);
     useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/posts')
         .then(res => res.json())
-        .then(data => setBlogs(data))
+        //.then(data => setBlogs(data))
     },[])
     return (
-        <div>
+        <div className="md:w-2/3 ">
+            <h1 className="text-4xl">Blogs: </h1>
             
         </div>
     );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
-
+import profile from '../../assets/images/profile.png'
 
 const Header = () => {
     return (
-        <div>
+        <header className='flex justify-between items-center p-5 border-b-2'>
              <h1 className='text-4xl font-bold'>Knoledge  Cafe</h1>
-        </div>
+             <img src={profile} alt="" />
+        </header>
     );
 };
 
